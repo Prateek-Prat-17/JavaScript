846a4c70953d265b98b84f3389f4f0fbcd4fe611
Prateek-Prat-17 <prateek17msd@gmail.com>
2026-03-08 03:06:08 +0530

    Ninth Commit

---
+++ ConcurEx13.2.js
@@ -0,0 +1,25 @@
+const promise = new Promise((resolve, reject) => {
+    resolve("Start Counting!");
+})
+.then (value => {
+    console.log(value);
+    return "One";
+})
+.then (value => {
+    console.log(value);
+    return "Two";
+})
+.then (value => {
+    console.log(value);
+    return "Three";
+})
+.then (value => {
+    console.log(value);
+    return "Four";
+})
+.then (value => {
+    console.log(value);
+})
+.catch (value => {
+    console.log(value);
+})
